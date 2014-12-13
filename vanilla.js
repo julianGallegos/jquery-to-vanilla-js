6070552b55cbb2f3e5c7f3d5a4da0916c019ddc6
julian.gallegos@gmail.com <julian.gallegos@gmail.com>
2014-12-13 00:24:14 -0800

    added two jquery search methods

---
+++ vanilla.js
@@ -0,0 +1,11 @@
+
+//jquery function to get all elements
+// for example get all divs would be $('div')
+
+var allDivs = document.getElementsByName('div')
+
+//jquery translation of getting element by class getting element by class getElementsByName
+// for example $('')
+
+var jQueryClassName = document.getElementsByClassName('inner');
+
